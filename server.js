92d8b1728f08898020d152227f12c790b71c1631
Ernestas Monkevicius <ernestas.monkevicius@ucdconnect.ie>
2018-12-29 09:46:22 +0000

    Binary upgrade to npc server

--- server.js
+++ server.js
@@ -29,6 +29,22 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
 });
+const eventMSG = Object.freeze({
+    player: Object.freeze({
+        list: "1",
+        new: "2",
+        disconnect: "3",
+        movement: "4",
+        moved: "5"
+    }),
+    star: Object.freeze({
+        location: "6",
+        collected: "7"
+    }),
+    score: Object.freeze({
+        update: "8"
+    })
+});
 
 io.on('connection', function (socket){
     console.log(`a user connected: ${socket.id}`); //DEBUGGING
@@ -45,10 +61,16 @@ io.on('connection', function (socket){
                 npcSocketID = npcSocket.io.engine.id;
             });
 
-            npcSocket.on('currentPlayers', function(players){
+            npcSocket.on(eventMSG.player.list, function(players){
                 //get the server generate player state for this bot
-                npcs[npcSocketID] = players[npcSocketID];
-                npcs[npcSocketID].socketHandle = npcSocket;
+                console.log(players[npcSocketID]); //testing
+
+                npcs[npcSocketID] = {
+                    binaryBuffer: players[npcSocketID], //Buffer instance
+                    socketHandle: npcSocket
+                };
+                /*npcs[npcSocketID].binaryBlobView = new DataView(players[npcSocketID]);
+                npcs[npcSocketID].socketHandle = npcSocket;*/
 
                 //console.log(npcs[npcSocketID]); //DEBUGGING
             });
@@ -85,18 +107,32 @@ io.on('connection', function (socket){
     });
 
     socket.on('moveBots', function(botsMovementState){
+        var updateRate = 16 * 3;
+
         if (botsMovementState.move){
             movementIntervalFunction = setInterval(function(){
                 //For each npc generate new npc pos and update the game server
                 Object.keys(npcs).forEach(function(npc){
-                    npcs[npc].rotation = npcs[npc].rotation + 0.01;
+                    /*npcs[npc].rotation = npcs[npc].rotation + 0.01;
                     //increment position or wrap around
                     npcs[npc].x = npcs[npc].x > 800 ? 0 : npcs[npc].x + 1;
-                    npcs[npc].y = npcs[npc].y > 550 ? 0 : npcs[npc].y + 1;
+                    npcs[npc].y = npcs[npc].y > 550 ? 0 : npcs[npc].y + 1;*/
 
-                    npcs[npc].socketHandle.emit('playerMovement', { x: npcs[npc].x, y: npcs[npc].y, rotation: npcs[npc].rotation});
+                    var currentNPC = npcs[npc];
+
+                    console.log(currentNPC.binaryBuffer.readInt16BE(0));
+                    var updatedRotation = currentNPC.binaryBuffer.readInt16BE(0) + 1;
+                    var updatedPositionX = currentNPC.binaryBuffer.readUInt16BE(2) > 800 ? 0 : currentNPC.binaryBuffer.readUInt16BE(2) + 1;
+                    var updatedPositionY = currentNPC.binaryBuffer.readUInt16BE(4) > 550 ? 0 : currentNPC.binaryBuffer.readUInt16BE(4) + 1;
+
+                    currentNPC.binaryBuffer.writeInt16BE(updatedRotation, 0);
+                    currentNPC.binaryBuffer.writeUInt16BE(updatedPositionX, 2);
+                    currentNPC.binaryBuffer.writeUInt16BE(updatedPositionY, 4);
+
+                    //currentNPC.socketHandle.emit(eventMSG.player.movement, movementToBinary({ x: npcs[npc].x, y: npcs[npc].y, rotation: npcs[npc].rotation}));
+                    currentNPC.socketHandle.emit(eventMSG.player.movement, currentNPC.binaryBuffer);
                 });
-            }, 16 * 3);
+            }, updateRate);
         } else clearInterval(movementIntervalFunction);
     });
 
@@ -110,3 +146,28 @@ io.on('connection', function (socket){
 server.listen(PORT, function(){
     console.log(`Listening on ${server.address().port}`);
 });
+
+var movementBinaryBlobView = new DataView(new ArrayBuffer(6)); //global scope for reuse
+function movementToBinary(movementObject){
+    /* input:
+    {   
+        x: this.ship.x,
+        y: this.ship.y,
+        rotation: this.ship.rotation
+    } 
+    */
+    /*var binaryBlob = Buffer.allocUnsafe(6); //6 bytes with cruft
+    
+    binaryBlob.writeUInt16BE(movementObject.rotation, 0);
+    binaryBlob.writeUInt16BE(movementObject.x, 2);
+    binaryBlob.writeUInt16BE(movementObject.y, 4);*/
+
+    //console.log("Rotation in movementToBinary::");
+    //console.log(movementObject.rotation);
+
+    movementBinaryBlobView.setInt16(0, movementObject.rotation*100);
+    movementBinaryBlobView.setUint16(2, movementObject.x);
+    movementBinaryBlobView.setUint16(4, movementObject.y);
+
+    return movementBinaryBlobView.buffer;
+}
